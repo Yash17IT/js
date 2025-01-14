bea9dc61923b1f49a36849b80a5eb7a76b16f818
Yash17IT <yashrane1715@gmail.com>
2025-01-14 09:28:28 +0000

    2nd video

---
+++ 01_basic/var.js
@@ -0,0 +1,23 @@
+const accountId =17725
+let accountemail ="yashrane@rkenc.edu"
+var accountpaasss = "1234"
+accountcity ="tumsar"
+let accountstatus
+//const can not be change
+
+// accountId = 2
+console.log(accountId);
+
+//error will come here as const is used accountid can not
+//changed to 2
+
+
+accountemail="YASH"
+accountpaasss = "56"
+accountcity="ngp"
+console.table([accountId,accountemail,accountpaasss,accountcity,accountstatus]);
+
+//prefer not tou use var
+//bcoz of isse in block scope and func scope
+
+
