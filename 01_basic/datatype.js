3a9b6b23bc054b1fd31991ff770efcb1ada43909
Yash17IT <yashrane1715@gmail.com>
2025-01-14 10:24:01 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,23 @@
+// alert(3+3)
+//error only run in browser not in nodejs
+console.log(3+3);
+//ecma tc39 and mdn contain standarads of js all datatypes theory and all
+
+//----------------------------------------------------------
+
+let name="yash"
+let age=3
+let isloggedin = false
+//string in ""
+// boolean = t or f
+//null = standalone value
+// undefined define hai par kahli hai
+// symbol =  for uniqueness
+
+//object = detail in next videos
+
+console.log(typeof age)
+//tell datatype of age heree
+//typeof null = object so null is object***
+// type of undefine = undefine he hai
+
