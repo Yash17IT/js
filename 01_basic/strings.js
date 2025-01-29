ebaa64086adabdffa7ec1159107f611bec5c317e
Yash17IT <yashrane1715@gmail.com>
2025-01-29 18:33:44 +0000

    str

---
+++ 01_basic/strings.js
@@ -0,0 +1,22 @@
+// let str = "yash";
+// let str2 = 'yash';
+// console.log(str.length);
+// str[0];
+// str[1];
+
+ //special string
+//  let obj={
+//     item :"pen"
+//     price:10
+
+//  };
+//  // ` is used so we not require ""and , in one line we can do it by `
+//  let op = `the cost of ${obj.item} is ${obj.price} rupees`;
+//  console.log(op);
+// //  -----------------------------------
+//  console.log("the cost of ",obj.item,"is",obj.price);
+ 
+//  let specialstring = `this a template`;
+//  console.log(typeof specialstring);
+ 
+
