6fe9d46174c5d592f2502910b1b38e7f249ffc4b
Yash17IT <yashrane1715@gmail.com>
2025-01-29 17:32:22 +0000

    operators

---
+++ 01_basic/operator.js
@@ -0,0 +1,54 @@
+// let i=1;
+// do{
+//     console.log("i=",i);
+//     i++;
+// }
+// while (i<=5);
+// //do while 
+// --------------------------------------------
+//for of loop
+// let str = "yash";
+// let length =0;
+// for(let i of str)
+// {
+//     console.log("i+",i);
+//     length++;
+    
+    
+    
+// }
+// console.log("length of string is:-",length);
+// -------------------------------------------------
+
+// //for in loop
+// let student
+// ={
+//     name: "yash",
+//     age: 19,
+//     cgpa:9.5,
+//     isPass: true,
+// };
+// for(let key in student)
+//     // for(let i in student)
+// {
+//     // keywords aate for in loop se
+//     // console.log(i);
+
+//     console.log("key=",key,"values=",student[key]);
+//     //key and both value get printed
+// }
+// -----------------------------------------------------------
+
+
+
+// practise que
+// Q1
+
+// // Q2
+// let gamenum = 25;
+// let usernum=prompt("guess the game number ");
+// while(usernum!=gamenum)
+// {
+//     usernum=prompt("you have guess the wrong game number ");
+// }
+// console.log("correct number");
