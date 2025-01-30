17794e55e9eb8f7eca78449b7a42d731e975a395
Yash17IT <yashrane1715@gmail.com>
2025-01-30 17:56:13 +0000

    hi

---
+++ 01_basic/array.js
@@ -0,0 +1,39 @@
+// let marks=[99,98,97];
+// marks.length;
+// let hero=["yash","yash"];
+// marks[0]=90;
+// console.log(marks);
+// //not for strings
+// //--------------------------------------
+
+//  let hero=["spiderman","ironman","thor"];
+// // for(let i=0; i<=hero.length;i++)
+// // {
+// //     console.log(hero);
+    
+// // }
+// //--------------------------------------------------
+
+// for(let i of hero )
+// {
+//     console.log(hero);
+    
+// // }
+
+// let city= ["delhi","pune","ngp"];
+// for(let i of city)
+// {
+//     console.log(city.toUpperCase());
+    
+// }
+//-------------------------------------------------------
+let marks=[85,97,44,37,76,60];
+let sum=0;
+for(let val of marks)
+{
+   
+sum += val;
+}
+console.log(sum);
+let avg = sum/marks.length;
+console.log(avg);
