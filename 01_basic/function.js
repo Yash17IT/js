a5e1507091fd4f3ffe48e20bf7166d78c29b895b
Yash17IT <yashrane1715@gmail.com>
2025-01-30 19:22:43 +0000

    h

---
+++ 01_basic/function.js
@@ -0,0 +1,29 @@
+// function sum(x,y)
+// {
+//     // console.log(x+y);
+//      s=x+y;
+//      return s;
+// }
+// let val = sum(9,9);
+// console.log(val);
+
+// // --------------------------------------------------------
+// function sum(a,b)
+// { 
+//     return a+b;
+// }
+// const arrowsum = (a,b) =>{
+//     console.log(a+b);
+    
+// }
+// function mul(a,b)
+// {
+//     return a*b;
+
+// }
+// const arrowmul = (a,b)=>{
+//     return a*b;
+// }
+
+//  -----------------------------------------------
+
